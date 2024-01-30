c2530a61201403943a778b6548699c71c7cbd431
AlessandroBilli <skillmanbungo@gmail.com>
2024-01-30 05:23:54 +0100

    feat: structure, to be fixed

--- routes/Homepage.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Http\Controllers\Guest\PageController;
-
-// La rotta '/' visualizza la lista di film
-Route::get('/movies', [PageController::class, 'movies'])->name('movies');
-
-// La rotta '/homepage' visualizza la tua homepage personalizzata
-Route::get('/homepage', [PageController::class, 'homepage'])->name('homepage');
